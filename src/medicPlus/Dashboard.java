17ed721ab202abe6e81523bbf4ce4780b9b609e1
gabriel-roque <gabrielrms.dll@gmail.com>
2018-11-20 06:59:46 -0200

    tela dashboard em construcao

--- src/medicPlus/Dashboard.java
+++ src/medicPlus/Dashboard.java
@@ -1,20 +1,97 @@
 package medicPlus;
 
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Dashboard {
 
 
     public void montarDashboard(JFrame f){
 
-        // modelo exemplo
+        String[] clientes = {"Gabriel Roque", "Jadilson Guedes", "Felipe Oliveira", "Lucas Roque"};
 
-        /*posso adicionar o componetes do dashboard direto aqui,
-        * pois ja tenho a minha referencia de frame*/
+        //LABEL Cliente
 
-        JButton b1 = new JButton();
-        b1.setBounds(60,5,20,30);
-        f.add(b1);
+        JLabel Lbclientes = new JLabel("Clientes");
+            Font fontCliente = new Font("SansSerif", Font.BOLD, 20);
+            Lbclientes.setFont(fontCliente);
+            Lbclientes.setBounds(10,10 ,120, 15);
+        f.add(Lbclientes);
+
+
+        //BUTTON Novo Cliente
+
+        JButton btNewCli = new JButton("Novo Cliente");
+            btNewCli.setBounds(355,10, 130,25);
+        f.add(btNewCli);
+
+        //SEPARATOR
+
+        JSeparator spDash = new JSeparator();
+            spDash.setBounds(10,40,475,15);
+        f.add(spDash);
+
+
+        //Coluna NOME
+
+        Font fontNome = new Font("SansSerif", Font.BOLD, 19);
+        JLabel nome = new JLabel("Nome");
+        nome.setFont(fontNome);
+            nome.setBounds(10,50,100,20);
+        f.add(nome);
+
+        Font fontNomeList = new Font("SansSerif", Font.ITALIC, 17);
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+
+            for (String view  : clientes){ //Exibe nomes
+
+                l1.addElement(view);
+
+            }
+
+        JList<String> list = new JList<>(l1);
+        list.setFont(fontNomeList); //font sytle
+        list.setBounds(10,80, 130,160);
+        f.add(list);
+
+        //SEPARATOR
+
+        JSeparator sp1 = new JSeparator(SwingConstants.VERTICAL);
+            sp1.setBounds(150,50, 15,190);
+        f.add(sp1);
+
+
+
+
+
+
+//        String data[][]={ {"101","Amit","670000"},
+//                {"102","Jai","780000"},
+//                {"101","Sachin","700000"}};
+//        String column[]={"ID","NAME","SALARY"};
+//        JTable tableCli = new JTable(data, column);
+//        tableCli.setBounds(10,70,400,150);
+//
+
+
+//        String data[][]={ {"101","Amit","670000"},
+//                {"102","Jai","780000"},
+//                {"101","Sachin","700000"}};
+//        String column[]={"ID","NAME","SALARY"};
+//        JTable jt=new JTable(data,column);
+//        jt.setBounds(120,40,200,100);
+//        JScrollPane sp=new JScrollPane(jt);
+//        f.add(jt);
+//
+////
+//        f.add(tableCli);
+
+
+
+
+
+
 
 
 
--- src/medicPlus/System.java
+++ src/medicPlus/System.java
@@ -17,7 +17,7 @@ public class System {
     public static void startSystem(){
 
         JFrame f = montarFrame();
-        montarFrame();
+        menuBar(f);
 
         Dashboard dash = new Dashboard();
         dash.montarDashboard(f);
@@ -33,9 +33,37 @@ public class System {
 
     }
 
+
+    private static void menuBar(JFrame f) {
+
+        JMenu menuArquivo = new JMenu("Arquivo");
+            JMenu subMenuSalvar = new JMenu("Salvar");
+                JMenuItem itemAbrir = new JMenuItem("Abrir");
+                JMenuItem itemSalvar = new JMenuItem("Salvar");
+                JMenuItem itemSalComo = new JMenuItem("Salvar como");
+                JMenuItem gr = new JMenuItem("Exportar PDF");
+        JMenu menuSair = new JMenu("Sair");
+
+        subMenuSalvar.add(itemSalvar);
+        subMenuSalvar.add(itemSalComo);
+        menuArquivo.add(itemAbrir);
+        menuArquivo.add(subMenuSalvar);
+        menuArquivo.addSeparator();
+        menuArquivo.add(gr);
+        menuArquivo.add(menuSair);
+
+
+        JMenuBar menuCompleto = new JMenuBar();
+        menuCompleto.add(menuArquivo);
+        menuCompleto.add(menuSair);
+        f.setJMenuBar(menuCompleto);
+
+    }
+
+
     public static void configFrame(JFrame f) {
 
-        f.setSize(385, 280);
+        f.setSize(500, 300);
         f.setResizable(false);
         f.setLayout(null);
         f.setVisible(true);
